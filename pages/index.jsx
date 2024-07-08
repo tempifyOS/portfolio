b2b9af01b6e17025ef20d5ed1a3dd604bfcb2446
TempifyOS <ryananthunt@gmail.com>
2024-07-08 17:04:27 -0500

    Add vercel analytics

--- pages/index.jsx
+++ pages/index.jsx
@@ -9,6 +9,7 @@ import Color 		from '../components/utils/page.colors.util'
 
 import colors 		from '../content/index/_colors.json'
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 
 //
 export default function HomePage() {
